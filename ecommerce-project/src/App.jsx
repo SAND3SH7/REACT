4c3ea21b675979f0996b9314519b9b387f2c26bd
Sandesh Shrestha <081bei027@ioepc.edu.np>
2026-05-13 20:20:17 +0545

    add surver proxy config for /api and /images

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,6 +1,7 @@
-
+import axios from 'axios';
 import {Routes,Route} from 'react-router';
 import {CheckoutPage} from './pages/CheckoutPage.jsx'
+import{useState,useEffect} from 'react';
 
 import './App.css'
 import HomePage from "./pages/HomePage.jsx"
@@ -10,12 +11,22 @@ import {Tracking} from './pages/Tracking';
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(()=>{
+  axios.get('/api/cart-items')
+            .then((response) => {
+                setCart(response.data);
+            })
+            .catch((error) => {
+                console.error('Failed to load cart items:', error);
+            });
+  },[]);
 
 
   return (
     <Routes>
-     <Route path="/" element={<HomePage/>} />
-     <Route path="/checkout" element={<CheckoutPage/>} />
+     <Route path="/" element={<HomePage cart={cart}/>} />
+     <Route path="/checkout" element={<CheckoutPage cart={cart}/>} />
      <Route path="/orders" element={<OrdersPage/>}/>
      <Route path="/tracking" element={<Tracking/>}/>
     </Routes>
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,22 +6,23 @@ import Header from '../components/Header';
 import './HomePage.css';
 
 
-export default function HomePage() {
+export default function HomePage({cart}) {
 
-    const[products,setProducts]=useState([]);
-    const[cart,setCart]=useState();
-   useEffect(()=>{
-        axios.get('http://localhost:3000/api/products')
-        .then((response)=>{
-        setProducts(response.data);
-        });
+    const [products, setProducts] = useState([]);
+  
 
-        axios.get('http://localhost:3000/api/cart-items')
-        .then((response)=>{
-            setCart(response.data);
-        })
+    useEffect(() => {
+        axios.get('/api/products')
+            .then((response) => {
+                setProducts(response.data);
+            })
+            .catch((error) => {
+                console.error('Failed to load products:', error);
+            });
 
-   },[]);
+        
+
+    }, []);
 
 
 
